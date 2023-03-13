6ca6c07de2a12fb18298002d864713736c3a0cc2
unknown <julifc2005@gmail.com>
2023-03-13 09:34:55 -0500

    aaaa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage { 
 
-  constructor() {}
+  tasks:{title:string;completed:boolean;}[]=
+  [
+    {title:"Tender la cama",completed:false},
+    {title:"",completed:false},
+    {title:"",completed:false}
+  ]
+  newTask='';
 
+  addTask(){
+    if(this.newTask.trim().length === 0){  //triple igual significa exactamente
+      return;
+    }
+    this.tasks.push ({title: this.newTask,completed:false});
+    this.newTask='';
+  }
+
+  deleteTask(task: {title: string; completed: boolean; }){
+    const index = this.tasks.indexOf(task);
+    this.tasks.splice(index,1);
+  }
+
+  getProgress() {
+    const totalTask = this.tasks.length;
+    const completedTask = this.tasks.filter(task => task.completed).length;
+    return completedTask / totalTask;
+  }
 }
